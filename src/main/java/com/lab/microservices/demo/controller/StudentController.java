d93487611dca0d8b7bab5659935fab9c135b647e
Rafa-Gamero <rafagameroarrabal@gmail.com>
2025-05-21 15:28:14 +0200

    solution lab

--- src/main/java/com/lab/microservices/demo/controller/StudentController.java
+++ src/main/java/com/lab/microservices/demo/controller/StudentController.java
@@ -1,6 +1,6 @@
 package com.lab.microservices.demo.controller;
 
-import com.lab.microservices.demo.model.Student;
+
 import com.lab.microservices.demo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,23 +8,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("/api/student")
 public class StudentController {
 
     @Autowired
     private StudentRepository studentRepository;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
-        return studentRepository.findById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    @GetMapping("/{studentId}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public ResponseEntity<?> getStudentById(@PathVariable Long studentId){
+        return (new ResponseEntity<>(studentRepository.findById(studentId), HttpStatus.OK));
     }
 
-    // Agrega este método para manejar POST
-    @PostMapping
-    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
-        Student savedStudent = studentRepository.save(student);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedStudent);
-    }
+
 }
